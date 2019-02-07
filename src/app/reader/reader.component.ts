743acf3a3594b84fbbae995799010bfd80a9da75
Adnan Mamajiwala <rawkz.star@gmail.com>
2019-02-07 15:27:27 -0600

    Initial commit

---
+++ src/app/reader/reader.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'ns-reader',
+    templateUrl: './reader.component.html',
+    styleUrls: ['./reader.component.css'],
+    moduleId: module.id,
+})
+export class ReaderComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
